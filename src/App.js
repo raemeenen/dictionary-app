8cf654dbcb9e9fc588f7c38ee749692728ae3d9d
raemeenen <107419219+raemeenen@users.noreply.github.com>
2022-08-27 14:02:39 -0500

    added search form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Dictionary App</h1>
-      </header>
+      <h1>Dictionary App</h1>
+      <Dictionary />
+      <footer className="App-footer">
+        <a href="https://github.com/raemeenen/dictionary-app">Open-source code</a> by Rachel Meenen
+      </footer>
     </div>
   );
 }
