cead160a0f18a837a41eef56c0287c51503ce65d
raemeenen <107419219+raemeenen@users.noreply.github.com>
2022-08-30 10:54:40 -0500

    Added Phonetic component with audio and text

--- src/Results.js
+++ src/Results.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetic from "./Phonetic";
 import Synonyms from "./Synonyms";
 
 export default function Results (props) {
@@ -7,6 +8,7 @@ export default function Results (props) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
+        <Phonetic phonetic={props.results.phonetics} />
         {props.results.meanings.map(function(meaning, index) {
           return <div key={index}>
             <Meaning meaning={meaning} />
