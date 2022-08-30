f1a80549214e491ad59d516314d76c0a6a938d0a
raemeenen <107419219+raemeenen@users.noreply.github.com>
2022-08-30 11:12:04 -0500

    Added audio conditional to Phonetic

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -2,13 +2,21 @@ import React from "react";
 
 export default function Phonetic (props) {  
   console.log(props.phonetic)
+  if (props.phonetic[0].audio) {
     return (
-    <div className="Phonetic">
-      <a href={props.phonetic[0].audio} target="_blank" rel="noreferrer">
-        Listen
-      </a>
-      <br />
-      {props.phonetic[0].text}
-    </div>
-  )
+      <div className="Phonetic">
+        <a href={props.phonetic[0].audio} target="_blank" rel="noreferrer">
+          Listen
+        </a>
+        <br />
+        {props.phonetic[0].text}
+      </div>
+    )
+  } else {
+    return (
+      <div className="Phonetic">
+        {props.phonetic[0].text}
+      </div>  
+    )
+  }
 }
