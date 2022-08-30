523d4b49d83b1a0abbba0a36334ae5c2399d4d24
raemeenen <107419219+raemeenen@users.noreply.github.com>
2022-08-30 11:22:34 -0500

    added conditional for the example in Meaning component

--- src/Meaning.js
+++ src/Meaning.js
@@ -5,17 +5,28 @@ export default function Meaning (props) {
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map (function (definition, index)
-      {
-        return (
-          <div key={index}>
-            <p>
-              <strong>Definition:</strong>{definition.definition}
-              <br />
-              <strong>Example:</strong><em>{definition.example}</em>
-              <br />
-            </p>
-          </div>
-        )
+        {if (definition.example) {
+          return (
+            <div key={index}>
+              <div>
+                <strong>Definition:</strong>{definition.definition}
+                <br />
+                <strong>Example:</strong><em>{definition.example}</em>
+                <br />
+                <br />
+              </div>
+            </div>
+          )
+        } else {
+          return (
+            <div key={index}>
+              <div>
+                <strong>Definition:</strong>{definition.definition}
+              </div>
+            </div>
+          )
+
+        }
       })}
     </div>
   )
